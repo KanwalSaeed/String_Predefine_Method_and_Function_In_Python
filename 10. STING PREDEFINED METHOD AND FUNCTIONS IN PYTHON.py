54d29917482f6b6233715b3e80813b8dd33b7fc2
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-04 23:14:38 +0500

    first code

---
+++ 10. STING PREDEFINED METHOD AND FUNCTIONS IN PYTHON.py
@@ -0,0 +1,57 @@
+
+
+
+message = "Hellow World"
+print(message.isupper())
+
+message = "HELLO WORLD"
+print(message.isupper())
+
+message = "HELLO WORLD"
+print(message.islower())
+
+message = "hello world"
+print(message.islower())
+
+message = "HELLO WORLD"
+print(message.lower())
+
+message = "HELLO WORLD"
+print(message.upper())
+
+message = "HELLO WORLD"
+print(message.lower().isupper())
+
+message = "hello world"
+print(message.upper().islower())
+
+message = "hello world"
+print(message.capitalize())
+
+message = "hello world"
+print(message.find('l'))
+
+message = "hello world"
+print(message.find('r'))
+
+message = "hello world"
+print(message.find("world"))
+
+message = "hello world"
+print(message.find('T'))
+
+name = "Kanwal Saeed"
+print(name.replace('Saeed', 'Chuhan'))
+
+message = "hello world"
+print(message.replace('h', 'J'))
+
+name = "Kanwal Saeed"
+print(len(name))
+
+message = "HELLO WORLD"
+print(len(message))
+
+
+
+
